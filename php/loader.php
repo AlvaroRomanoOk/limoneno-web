3609195c518582f5c1a38c91f69d5eaf9db6683e
Alvaro Romano <alvaroromanook@gmail.com>
2022-10-08 14:08:05 -0300

    reload

--- php/loader.php
+++
@@ -1,8 +0,0 @@
-<!-- CSS -->
-<link rel="stylesheet" href="./css/loader.css">
-
-<!-- LOADER DE LA PÁGINA -->
-
-<div class="loader" id="loader">
-    <img id="loaderimg" src="assets/loader.png">
-</div>
